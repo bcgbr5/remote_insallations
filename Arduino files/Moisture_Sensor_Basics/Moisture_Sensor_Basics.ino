7e3461c3e71fdc129b9b744c390ed4bbe606a043
Brandon Greer <brandonchrisgreer@gmail.com>
2019-10-04 21:20:37 -0500

    Isolated moisture sensor code and started adding GPS/Location flags to the code for future use

--- Arduino files/Moisture_Sensor_Basics/Moisture_Sensor_Basics.ino
+++ Arduino files/Moisture_Sensor_Basics/Moisture_Sensor_Basics.ino
@@ -12,7 +12,11 @@ Current Sketch assumes that you have a capacitive moisture sensor with a range f
                   //There should also eventually be a setting for non-Lora modules that exist on thier own
                   //and (ideally) a set for non-gsm modules that operate over LoRaWAN
 
-#define MOI_MAX = 846//The maximum capacitive value you have observed for the current moisture sensor completly dry
+#define HAS_SENSOR 1//On the offchance for some reason a GSM enabled unit doesn't have a moisture sensor (for example if it's only acting as a relay for another one in an area with no coverage), 
+                    //we'll keep the moisture sensor code inside of one of these. This will also make switching out to the I2C style or resistive style sensors later easier,by allowing another value if that becomes neccesary.
+#define HAS_GPS 1//This unit has GPS
+//#define LOCATION_CODE //TODO This will need to be defined either as a numerical value that the recieving backend will understand, or as an array of [LAT, LONG]. Either this or the HAS_GPS flag need to be enabled
+#define MOI_MAX = 846//The maximum capacitive value you have observed for the current moisture sensor completly dry. Due to varience in the sensors and wires, this may vary wqith the unit.
 #define MOI_MIN = 371//The minimum value observed by submerging the sensor to it's depth line in water
 
 //for feather m0 RFM9x
@@ -21,8 +25,6 @@ Current Sketch assumes that you have a capacitive moisture sensor with a range f
 #define RFM95_INT 3
 //Must match all RX's freq!
 #define RF95_FREQ 915.0
-// Blinky on receipt
-#define LED 13
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
@@ -71,13 +73,15 @@ void setup() {
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void loop() {
-  moisture = analogRead(0);//Anolog reading of capaicitive moisture sensor
-  Serial.print("\nMoisture: ");
-  Serial.print(moisture);
-  delay(500);
+  if(HAS_SENSOR){
+    moisture = analogRead(0);//Anolog reading of capaicitive moisture sensor
+    Serial.print("\nMoisture: ");
+    Serial.print(moisture);
+    delay(500);
+  }
 
 
-  if(!MASTER){
+  if(!MASTER){//This designates this unit as just a moisture sensor that will transmit to the master with the GSM module
     Serial.println("Sending to rf95_server");
     // Send a message to rf95_server
     
@@ -118,7 +122,7 @@ void loop() {
     }
     delay(1000);
   }
-  else if(MASTER){
+  else if(MASTER){//Designates unit as having a GSM module.
     // Should be a message for us now   
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
